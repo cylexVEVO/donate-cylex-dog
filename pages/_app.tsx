20dbc97de23cd4f628fdae36c625ca12d48e9f64
cylex <cylex@cylex.dog>
2022-06-05 10:14:36 -0400

    Add meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,18 @@
 import * as React from "react";
 import type {AppProps} from "next/app";
 import "../styles.scss";
+import Head from "next/head";
 
 const App: React.FC<AppProps> = ({Component, pageProps}) => {
-	return <Component {...pageProps}/>;
+	return (
+		<>
+			<Head>
+				<title>Donate using my money</title>
+				<meta name="description" content="Click this button a bunch of times and make me spend money."/>
+			</Head>
+			<Component {...pageProps}/>
+		</>
+	);
 };
 
 export default App;
